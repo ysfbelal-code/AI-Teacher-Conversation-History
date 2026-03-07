b3cd099e6e98ca160fcc96456c711454035674f0
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-07 10:27:14 +0300

    Update API keys in config.py
    Updated HF_API_KEY and GROQ_API_KEY with new values.

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-HF_API_KEY = os.getenv("HF_API_KEY", "")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
+HF_API_KEY = os.getenv("HF_API_KEY", "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")
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
