e8546d011f331cd51bb4abd8c4dc874cb43f407e
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-07 10:26:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py (Streamlit)
 # Switch provider by changing the import line:
-from groq import generate_response
+from hf import generate_response
 # from groq import generate_response
 
 import io
@@ -70,4 +70,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
