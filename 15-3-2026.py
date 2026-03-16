77d63676a054d48033358870e24f9979bad2ac5a
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-16 10:29:03 +0300

    Update 15-3-2026.py

--- 15-3-2026.py
+++ 15-3-2026.py
@@ -13,7 +13,7 @@ if language:
 
 title_prompt = f"""Only output this title - NOTHING else. 
 It's crucial you musn't change ANYTHING, otherwise it will cause confusion among users. 
-Translate the heading to {st.session_state['language']} if language isn't English. 
+Translate the heading to {language}. 
 Don't change ANYTHING from the text either and AVOID repetition at all costs."""
 
 st.set_page_config(page_title=generate_response(title_prompt + "AI MATH MASTERMIND"), layout='centered')
