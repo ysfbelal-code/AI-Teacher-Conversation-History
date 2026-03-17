9a3dc43c6fb0a2c13790c0f4a2d0bb644ef955b9
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-17 11:04:17 +0300

    Update 15-3-2026.py

--- 15-3-2026.py
+++ 15-3-2026.py
@@ -28,11 +28,17 @@ else:
 if language in rtl:
     st.markdown("""
     <style>
-    .stMarkdown p, .stMarkdown li, .stMarkdown pre,
-    .stExpander, .stExpander *,
-    [data-testid="stExpander"], [data-testid="stExpander"] * {
-        direction: rtl !important;
-        text-align: right !important;
+    .stMarkdown ul, .stMarkdown ol {
+    direction: rtl !important;
+    text-align: right !important;
+    padding-right: 1.5rem !important;
+    padding-left: 0 !important;
+}
+
+.stMarkdown ul li, .stMarkdown ol li {
+    direction: rtl !important;
+    text-align: right !important;
+}
     }
     </style>
     """, unsafe_allow_html=True)
