91f0959df172825709d8e23ddbeaff11fdc172d0
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-03-17 10:33:39 +0300

    Update 15-3-2026.py

--- 15-3-2026.py
+++ 15-3-2026.py
@@ -63,7 +63,7 @@ header3 = generate_response(title_prompt + "Export conversation history")
 
 st.markdown(f"<h1 style='text-align:center'>{title}</h1>", unsafe_allow_html=True)
 with st.expander("Examples of problems I can solve:"):
-    st.markdown(
+    st.markdown(generate_response(title_prompt + 
 """
 Probability - There are 17 counters in a bag,
 4 of the counters are red and the rest are blue.
@@ -73,10 +73,10 @@ Find the probability that the counter is blue
 Algebra - Simplify (x-7)(x+9)
 
 Trigonometry - ABC is a right-angled triangle.
-The height - AB - is 13cm and the hypotenuse - BC - is 13cm.
+The height - AB - is 12cm and the hypotenuse - BC - is 13cm.
 Calculate angle x at the base.
 
-Discrete Maths - If a set B has n elements, then what is the total number of subsets of B? Justify your answer.""")
+Discrete Maths - If a set B has n elements, then what is the total number of subsets of B? Justify your answer."""))
     
 c1, c2, c3 = st.columns([1,1,1])
 with c1:
